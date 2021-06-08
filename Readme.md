75ff138f45b5cc1c228db3dbc78bbe7ae827e005
ryan.p <ryan.p@endurance.com>
2021-06-08 10:44:36 +0530

    Skeleton

---
+++ Readme.md
@@ -0,0 +1,5 @@
+#PSD to html
+
+
+#SASS command
+ sass sass/index.scss css/index.css
---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+console.log("working");
+
+$("#Features-tabs ul li").on("click", function () {
+    $('li ').removeClass("active");
+    // $( this ).find( 'li.active' ).removeClass( 'active' );
+    var x = $(this).find('.active');
+    $(this).addClass('active');
+    $(".tabs-content").hide();
+    $("div[id=" + $(this).attr("data-tab-type") + "]").show();
+    console.log(x);
+});
